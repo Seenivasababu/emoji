e40ffd26c44658552655d76b746b9251d8eb0a24
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-12-20 21:26:46 +0530

    Post displayed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,21 @@ import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const posts = await api.post.getAll.query()
   const {  userId } = auth();
   if(!userId) return <UserButton afterSignOutUrl="/"/>
 
   const user = await currentUser()
-  console.log(user);
+
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
       Hello{`${user?.firstName}`}
       {userId && <UserButton afterSignOutUrl="/"/>}
+      {posts.map(post=>{
+        return <div key={post.id}> {post.content}</div>
+      })}
     </main>
   );
 }
 
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
-  );
-}
